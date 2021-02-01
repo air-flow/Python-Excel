49acb2bdb824b7defd607d008276fbf49a88d610
辻気流 <kiryu.18011106@gmail.com>
2021-02-01 21:51:19 +0900

    fix:The answer flag  isn't working.

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 import openpyxl
-# import pprint
+import pprint
 import re
 import random
 # 問題文が複数行に渡り記述してある
@@ -56,7 +56,7 @@ class Question:
         self.answer = []
         self.row = None
         self.col = None
-        self.choices_flag = None  # 正解が設定されているか判定
+        self.choices_flag = False  # 正解が設定されているか判定
 
     def _SetQuestion(self, text):
         if text is not None:
@@ -83,10 +83,10 @@ class Question:
             self.no = no
 
     def _CheckAnswerCount(self):
-        if len(self.choices) < 1:
-            self.choices_flag = False
-        else:
+        if len(self.choices) > 0:
             self.choices_flag = True
+        else:
+            self.choices_flag = False
 
     def _CheckBold(self, cell, text):
         flag = cell.font.bold
@@ -141,7 +141,16 @@ class AWSCloudPractitioner:
         self.setting_question = []
         self.mode = True
         self.question_Definition = {"q": "", "a": "^[a-zA-Z][.]"}
+        self.number_of_correct_answers = 0
+        self.print_mode = 0
         # self.reorganization_list = None
+
+    def _CheckAnswerSreach(self):
+        # print(len(self.question_list))
+        # print(self.question_list[209]._QuestionResultPrint())
+        for i in self.question_list:
+            if not i.choices_flag:
+                i._QuestionResultPrint()
 
     def _ReorganizationQuestionList(self):
         # question_flag = False
@@ -189,6 +198,7 @@ class AWSCloudPractitioner:
             else:
                 print("answer not set error")
             self.setting_question.append(select)
+        self._CheckingAnswers()
 
     def _InputUserAnswer(self):
         print("解答を選択してください")
@@ -207,6 +217,39 @@ class AWSCloudPractitioner:
         if self.mode:
             question._ProblemPrint()
 
+    def _CheckingAnswers(self):
+        for i in self.setting_question:
+            answer_list = i["obj"].answer
+            answer = self._ReplaceAnswerList(answer_list)
+            choice = i["choices"].replace(".", "").replace(",", "")
+            choice = list(self._ReplaceAlphabetString(choice))
+            answer = self._SortListAlphabet(answer)
+            choice = self._SortListAlphabet(choice)
+            if answer == choice:
+                self.number_of_correct_answers += 1
+        self._QuestionResult()
+
+    def _QuestionResult(self):
+        self.setting_question[0]["obj"]._HaihunPrint("結果")
+        print(str(self.number_of_correct_answers) +
+              "/" + str(len(self.setting_question)))
+
+    def _ReplaceAnswerList(self, answer_list):
+        result = ""
+        for i in answer_list:
+            result += i[0]
+        return list(result)
+
+    def _ReplaceAlphabetString(self, text):
+        alphabet = [chr(i) for i in range(65, 65 + 26)]
+        number = list(map(str, list(range(1, 10))))
+        d = dict(zip(number, alphabet))
+        # print(s.translate(str.maketrans(d)))
+        return text.translate(str.maketrans(d))
+
+    def _SortListAlphabet(self, sort_list):
+        return sort_list.sort()
+
 
 def cd():
     import os
@@ -217,17 +260,17 @@ def main():
     ex = Execl("../mine/path.txt")
     ex._GetFileExcelPath()
     ex._GetExcelFile()
-    # ex._ExcelGetCell(0)
     for i in range(ex.sheet_count):
         ex._ExcelGetCell(i)
-    # print(len(ex.text))
     aws = AWSCloudPractitioner(ex.text)
     aws._ReorganizationQuestionList()
     # print(aws.question_list[0])
     # print(len(aws.question_list))
     # for i in range(10):
     #     print(i, "番目", ex.text[i])
-    aws._MockExamination(3)
+    # aws._MockExamination(1)
+    print(aws.question_list[210].choices_flag)
+    aws._CheckAnswerSreach()
     # for i in range(3):
     #     temp = random.choice(aws.question_list)
     #     temp._QuestionPrint()
@@ -238,3 +281,5 @@ def main():
 if __name__ == "__main__":
     cd()
     main()
+    # pprint.pprint(d)
+    # print(alphabet, number)
